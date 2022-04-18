811f7256eca6caa658c2686b390c984b39f3ffdc
alexeyosipov88 <aoosipov88@gmail.com>
2022-04-17 23:09:25 -0400

    graph clone update

--- clone-graph.java
+++ clone-graph.java
@@ -2,8 +2,8 @@
 // Definition for a Node.
 class Node {
     public int val;
-    public List<NoNodede> neighbors;
-    public () {
+    public List<Node> neighbors;
+    public Node() {
         val = 0;
         neighbors = new ArrayList<Node>();
     }
@@ -19,59 +19,25 @@ class Node {
 */
 
 class Solution {
-  boolean[] marked = new boolean[100];
-  Node result;
-  
-  HashMap<Integer, List<Integer>> adjList = new HashMap<>();
-  
-  public Node cloneGraph(Node node) {
-      Node result = new Node();
-      getAllNeighbors(node);
-      System.out.println(adjList);
-      result = createNode();
-      return result;
-  }
-  
-  public void getAllNeighbors(Node node) {
-      if(marked[node.val]) return;
-      marked[node.val] = true;
-      List<Integer> neigborNodes = new ArrayList<>();
-      for(int i = 0; i < node.neighbors.size(); i++) {
-          neigborNodes.add(node.neighbors.get(i).val);
-      }
-      adjList.put(node.val, neigborNodes);
-      for(int i = 0; i < node.neighbors.size(); i++) { 
-          getAllNeighbors(node.neighbors.get(i));
-      }
-  
-  }
-  
-  public Node createNode() {
-      
-      Node[] hash = new Node[100];
-      for(int i = 0; i < hash.length; i++) {
-          hash[i] = null;
-      }
-  
-      
-      List<Node> nodeList = new ArrayList<>();
-      
-      for (Map.Entry<Integer, List<Integer>> entry : adjList.entrySet()) {
-          Integer key = entry.getKey();
-          Node node = new Node(key);
-          nodeList.add(node);
-          hash[key] = node;
-      } 
-      
-      for(Node node : nodeList) {
-          List<Integer> neighbors = adjList.get(node.val);
-          for(Integer i : neighbors) {
-              node.neighbors.add(hash[i]);
-          }
-      }
-      return nodeList.get(0);
-  
-  }
-  
-  
+    
+    HashMap<Node, Node> hash = new HashMap();
+    
+    public Node cloneGraph(Node node) {
+        if(node == null) {
+            return node;
+        }
+         if(hash.containsKey(node)) {
+            return hash.get(node);
+        }
+        Node clonedNode = new Node(node.val, new ArrayList<Node>());
+        hash.put(node, clonedNode);
+        for(Node neighbor : node.neighbors) {
+            System.out.println("iteration for " + node.val + " " + neighbor.val);
+            clonedNode.neighbors.add(cloneGraph(neighbor));
+            System.out.println("After iteration for " + node.val + " " + neighbor.val);
+        }
+        System.out.println("FINAL RETURN for " + node.val);
+        return clonedNode;
+        
+    }
 }
