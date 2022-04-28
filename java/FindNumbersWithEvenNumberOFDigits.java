233b0deb9fa532974fe17bcb352679b9294cc35f
alexeyosipov88 <aoosipov88@gmail.com>
2022-04-28 17:40:52 -0400

    add find numbers with even digits

---
+++ java/FindNumbersWithEvenNumberOFDigits.java
@@ -0,0 +1,20 @@
+class FindNumbersWithEvenNumberOFDigits {
+
+  public int findNumbers(int[] nums) {
+    int result = 0;
+    for(int i = 0; i < nums.length; i++) {
+        int numberOfDigits = 1;
+        int current = nums[i];
+        while(current / 10 >= 1) {
+            current = current / 10;
+            numberOfDigits++;
+            
+        }
+        if(numberOfDigits % 2 == 0) result++;
+    }
+    
+    return result;
+}
+
+
+}
