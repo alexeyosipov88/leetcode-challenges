73c7fa52703cf693e6fe8c0df5ab148eeb680158
alexeyosipov88 <aoosipov88@gmail.com>
2022-04-28 17:12:19 -0400

    add max consecutive ones

---
+++ java/MaxConsecutiveOnes.java
@@ -0,0 +1,24 @@
+class MaxConsecutiveOnes {
+
+
+
+// mySolution 
+//   public int findMaxConsecutiveOnes(int[] nums) {
+//     int maxSum = 0;
+//     int currentSum = 0;
+//     for(int i = 0; i < nums.length; i++) {
+//         if(nums[i] != 1) currentSum = 0; 
+//         else currentSum++;
+//         if(currentSum > maxSum) {
+//             maxSum = currentSum;  
+//         }
+//     }
+//     return maxSum;
+    
+// }
+
+
+
+
+
+}
