5811a523cdbcaad469a9609c12b4340656e4673e
alexeyosipov88 <aoosipov88@gmail.com>
2022-05-01 22:29:11 -0400

    add dublicate zeroes

--- java/DublicateZeroes.java
+++ java/DublicateZeroes.java
@@ -1,6 +1,33 @@
 class DuplicateZeroes {
 
 
+  public void duplicateZeros(int[] arr) {
+    int possibleZeroes = 0;
+    int realLength = arr.length;
+    int originalLast = arr.length - 1;
+
+    for(int i = 0; i < arr.length - possibleZeroes; i++) {
+        if(arr[i] == 0) {
+            realLength--;
+            if(i + 1  == arr.length - possibleZeroes) {
+                arr[originalLast] = 0;
+                originalLast--;
+                break;
+            }
+            possibleZeroes++;
+        }          
+    }
+    for(int last = realLength - 1; last >= 0; last--) {
+        arr[originalLast] = arr[last];
+        if(arr[last] == 0) {
+            originalLast--;
+            arr[originalLast] = 0;
+        }
+        originalLast--;   
+    }
+
+    
+}
   // tricky O(1) solution
 //   public void duplicateZeros(int[] arr) {
 //     int possibleZeroes = 0;
