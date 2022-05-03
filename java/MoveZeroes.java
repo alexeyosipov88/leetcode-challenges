88f496ad3d6b4ce92bb8359e0777b124acdcc69e
alexeyosipov88 <aoosipov88@gmail.com>
2022-05-03 16:00:02 -0400

    improve move zeroes

--- java/MoveZeroes.java
+++ java/MoveZeroes.java
@@ -2,28 +2,43 @@ class MoveZeroes {
 
   class Solution {
     
+
     public void moveZeroes(int[] nums) {
         int j = 0;
-        int numberOfZeroes = 0;
-        
-        
         for(int i = 0; i < nums.length; i++) {
             if(nums[i] != 0) {
                 nums[j++] = nums[i];
             }
-            if(nums[i] == 0) {
-                numberOfZeroes++;
-            }
-        }
-        
-        for(int i = nums.length - 1; i >= nums.length - numberOfZeroes; i--) {
-            nums[i] = 0;
             
         }
-        
+       for(int i = j; i < nums.length; i++) {
+           nums[i] = 0; 
+       }
         
     }
-}
+
+//     public void moveZeroes(int[] nums) {
+//         int j = 0;
+//         int numberOfZeroes = 0;
+        
+        
+//         for(int i = 0; i < nums.length; i++) {
+//             if(nums[i] != 0) {
+//                 nums[j++] = nums[i];
+//             }
+//             if(nums[i] == 0) {
+//                 numberOfZeroes++;
+//             }
+//         }
+        
+//         for(int i = nums.length - 1; i >= nums.length - numberOfZeroes; i--) {
+//             nums[i] = 0;
+            
+//         }
+        
+        
+//     }
+// }
 
 
 
