3cc2c5f8c8d70a2fca2ef6fa1fe003b8c5e497c3
alexeyosipov88 <aoosipov88@gmail.com>
2022-05-03 00:41:56 -0400

    add better solution for replace elements with greatest elements on right side

--- java/ReplaceElementsWithGreatestElementOnRightSide.java
+++ java/ReplaceElementsWithGreatestElementOnRightSide.java
@@ -2,21 +2,37 @@ class ReplaceElementsWithGreatestElementOnRightSide {
 
 
   class Solution {
+
     public int[] replaceElements(int[] arr) {
-        for(int i = 0; i < arr.length - 1; i++) {
-            int biggest = arr[i + 1];
-            for(int j = i + 2; j < arr.length; j++) {
-                if(arr[j] > biggest) {
-                    biggest = arr[j];
-                }
-            }
-            arr[i] = biggest;
+      int biggest = arr[arr.length - 1];
+      for(int i = arr.length - 2; i >= 0; i--) {
+           int current = arr[i];
+           arr[i] = biggest;
+           if(current > biggest) {
+              biggest = current;
+           }  
+      }
+      arr[arr.length - 1] = -1;
+      return arr;
+  }
+
+    
+    // my first attemp
+    // public int[] replaceElements(int[] arr) {
+    //     for(int i = 0; i < arr.length - 1; i++) {
+    //         int biggest = arr[i + 1];
+    //         for(int j = i + 2; j < arr.length; j++) {
+    //             if(arr[j] > biggest) {
+    //                 biggest = arr[j];
+    //             }
+    //         }
+    //         arr[i] = biggest;
         
-        }
-            arr[arr.length - 1] = -1;
+    //     }
+    //         arr[arr.length - 1] = -1;
         
-        return arr;
-    }
+    //     return arr;
+    // }
 }
 
 }
