75f5fdcdcf7dbd879718993e7e97fa2786ee1437
alexeyosipov88 <aoosipov88@gmail.com>
2022-05-11 14:13:36 -0400

    recursive palindrome linked list

--- java/PalindromeLinkedList.java
+++ java/PalindromeLinkedList.java
@@ -1,22 +1,51 @@
 class PalindromeLinkedList {
 
-  public boolean isPalindrome(ListNode head) {        
-    ArrayList<Integer> arr = new ArrayList<>();
-    ListNode current = head;
+  
+
+  
+// recursive 
+
+private ListNode current;
     
-    while(current != null) {
-        arr.add(current.val);
-        current = current.next;
-    }
-    int j = arr.size() - 1;
-    for(int i = 0; i < arr.size(); i++) {
-        if(j == i) return true;
-        if(arr.get(i) != arr.get(j - i)) {
-            return false;
-        }
-    }
-    return true;
+public boolean isPalindrome(ListNode head) { 
+    
+    current = head;
+    return reversedLinkedList(head); 
+    
 }
+
+public boolean reversedLinkedList(ListNode head) {
+   if(head == null) {
+       return true;
+   }
+   boolean result = reversedLinkedList(head.next);
+
+   if(current.val != head.val) {
+       return false;
+   }        
+   current = current.next;
+   return result;
+}
+
+
+
+//   public boolean isPalindrome(ListNode head) {        
+//     ArrayList<Integer> arr = new ArrayList<>();
+//     ListNode current = head;
+    
+//     while(current != null) {
+//         arr.add(current.val);
+//         current = current.next;
+//     }
+//     int j = arr.size() - 1;
+//     for(int i = 0; i < arr.size(); i++) {
+//         if(j == i) return true;
+//         if(arr.get(i) != arr.get(j - i)) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
 
 
 }
